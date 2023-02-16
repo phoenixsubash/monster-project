fcada24cf1477b6580305d6e1931292a43c65621
phoenixsubash <poudelsubash1234@gmail.com>
2023-02-16 22:02:55 +0545

    Feat: card-component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card } from "../card/card.components";
-import "./card-list.styles.css"
+import "./card-list.styles.css";
 
-export const Cardlist = props => {
-    return <div className="card-list">
-          {props.monsters.map((monsters) => (
-          <Card key={monsters.id} monster = {monsters}/>
-          ))}
-        </div>
-}
+export const Cardlist = (props) => (
+  <div className="card-list">
+    {props.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
